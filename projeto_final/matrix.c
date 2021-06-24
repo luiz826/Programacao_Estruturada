3c57cc62fe71b6b9879c962f8ca69faa3f122db3
Luiz Fernando <62849389+luiz826@users.noreply.github.com>
2021-06-24 15:32:01 -0300

    Add files via upload

--- projeto_final/matrix.c
+++ projeto_final/matrix.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 //#include "matrix.h"
 
+
+
 struct matrix {
-int *data;
-int n_rows;
-int n_cols;
-int stride_rows;
-int stride_cols;
-int offset;
+	int *data;
+	int n_rows;
+	int n_cols;
+	int stride_rows;
+	int stride_cols;
+	int offset;
 };
+
+
 
 // EM TEORIA ISSO TA CERTO
 struct matrix create_matrix(int *data, int n_rows, int n_cols) {
 	struct matrix b;
+
 	b.data = data;
 	b.n_rows = n_rows;
 	b.n_cols = n_cols;
@@ -24,32 +30,50 @@ struct matrix create_matrix(int *data, int n_rows, int n_cols) {
 	return b;
 }
 
-// ERRADO. PQ TA ERRADO?????
-struct matrix zero_matrix(int n_rows, int n_cols){
-	int dim;
-	struct matrix c;
 
-	dim = n_rows * n_cols;
+// ERRADO. PQ TA ERRADO????? - Deu certo assim
+struct matrix zero_matrix(int n_rows, int n_cols) {
+	int dim = (n_rows*n_cols);
+	int ar[dim];
 
-	int array[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-	
-	for (int i=0; i<9; i++){
-		printf("%d, ", array[i]);
-	}
-	c = create_matrix(array, n_rows, n_cols);
+	memset(ar, 0, dim*sizeof(ar[0]));
 
-	return c;
+	return create_matrix(ar, n_rows, n_cols);
 }
+
+
+// Deu certo tbm (primeiro eu fiz a matriz de zeros[não usei o 
+// zero_matrix pq deu merda] e depois coloquei 1 na diagonal principal)
+struct matrix i_matrix(int n) {
+	int dim = n*n;
+	int ar[dim];
+
+	memset(ar, 0, dim*sizeof(ar[0]));
+
+	for (int i=0; i<dim; i = i+(n+1)){
+		ar[i] = 1;
+	}
+
+	return create_matrix(ar, n, n);
+}
+
+
+
+/////////////////////////////////////////////////////////////////////
+////////////////////////////////MAIN/////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 
 // FUNÇÃO MAIN PARA TESTAR O QUE TA DANDO ERRADO
 int main(void){
+///////////////////////////TESTE1////////////////////////////////////
+
 	struct matrix a;
-	//int array[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int array[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-	a = zero_matrix(3, 3);
+	a = create_matrix(array, 3, 3);
 
-	for (int i=0; i<9; i++){
-		printf("%d, ", a.data[i]);
+	for (int i=0; i<(a.n_rows*a.n_cols); i++){
+	  	printf("%d, ", a.data[i]);
 	}
 
 	printf("\n");
@@ -58,7 +82,41 @@ int main(void){
 	printf("%d\n", a.stride_rows);
 	printf("%d\n", a.stride_cols);
 	printf("%d\n", a.offset);
+	printf("\n");
+//////////////////////////TESTE2////////////////////////////////////
 
+	struct matrix b;	
+
+	b = zero_matrix(3, 3);
+
+	for (int i=0; i<(b.n_rows*b.n_cols); i++){
+	  	printf("%d, ", b.data[i]);
+	}
+
+	printf("\n");
+	printf("%d\n", b.n_rows);
+	printf("%d\n", b.n_cols);
+	printf("%d\n", b.stride_rows);
+	printf("%d\n", b.stride_cols);
+	printf("%d\n", b.offset);
+	printf("\n");
+//////////////////////////TESTE3////////////////////////////////////
+
+	struct matrix c;	
+
+	c = i_matrix(3);
+
+	for (int i=0; i<(c.n_rows*c.n_cols); i++){
+	  	printf("%d, ", c.data[i]);
+	}
+
+	printf("\n");
+	printf("%d\n", c.n_rows);
+	printf("%d\n", c.n_cols);
+	printf("%d\n", c.stride_rows);
+	printf("%d\n", c.stride_cols);
+	printf("%d\n", c.offset);
+	printf("\n");
 
 	return 0;
 }
